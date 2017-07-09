9214df84511fe29833901b6585d41298c7237819
Tatyana Pomaz <pomazts@gmail.com>
2017-07-09 11:58:20 +0300

    one more example of pattern Observer

---
+++ src/observer/jobsite/Observed.java
@@ -0,0 +1,9 @@
+package observer.jobsite;
+
+public interface Observed {
+    void addObserver(Observer observer);
+
+    void removeObserver(Observer observer);
+
+    void notifyObservers();
+}
---
+++ src/observer/jobsite/Observer.java
@@ -0,0 +1,7 @@
+package observer.jobsite;
+
+import java.util.List;
+
+public interface Observer {
+    void handleEvent(List<String> vacancies);
+}
